f5385e94ffbe893e284416bdd0a5fc6bccac5534
Tomasz Bąk <tomaszbakk@outlook.com>
2017-07-08 22:36:16 +0200

    Coś się zepsuło z branchami. Musiałem dodać na nowo projekt MetSide

---
+++ MetCounterService/MetSide/settings.py
@@ -0,0 +1,10 @@
+import logging
+
+def init():
+    global workfolder
+    global GlobalMongoDatabaseAddress
+    global LocalMongoDatabaseAddress
+
+    workfolder = '/home/tomek/metcounter/'
+    GlobalMongoDatabaseAddress = '***REMOVED***'
+    LocalMongoDatabaseAddress = '192.168.1.246'
