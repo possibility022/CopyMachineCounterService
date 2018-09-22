78b557ca4db936f5376af639bedc9c9518d44219
Tomasz Bąk <tomaszbakk@outlook.com>
2018-09-22 22:16:01 +0200

    Working connection!

---
+++ MetCounterService/CopyinfoWPF.ORM.Tests/NHibernateConnection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CopyinfoWPF.ORM.Tests
+{
+    [TestClass]
+    public class NHibernateConnection
+    {
+        [TestMethod]
+        public void TestConnection()
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "Server=WIN-RP56U0UJDMQ;Initial Catalog=MetCounterService;User Id=Superuser;Password=1234567890";
+                x.Dialect<NHibernate.Dialect.MsSql2012Dialect>();
+            }
+            );
+
+            cfg.AddDeserializedMapping(ConfigurationSettings.GetMapping(), null);
+
+            var sessionFactory = cfg.BuildSessionFactory();
+
+            IEnumerable<Emailsource> list;
+
+            using (var session = sessionFactory.OpenSession())
+            {
+                list = session.Query<Emailsource>().Take(10).ToList();
+            }
+
+            foreach (var el in list)
+                Console.WriteLine(el.Id);
+
+        }
+    }
+}
---
+++ MetCounterService/CopyinfoWPF.ORM/ConfigurationSettings.cs
@@ -0,0 +1,25 @@
+﻿using CopyinfoWPF.ORM.Maps;
+using NHibernate.Cfg.MappingSchema;
+using NHibernate.Mapping.ByCode;
+
+namespace CopyinfoWPF.ORM
+{
+    public static class ConfigurationSettings
+    {
+
+        public static HbmMapping GetMapping()
+        {
+            var mapper = new ModelMapper();
+            mapper.AddMapping<EmailsourceMap>();
+
+            var mapping = mapper.CompileMappingFor(
+                new[]
+                {
+                    typeof(Emailsource)
+                });
+
+            return mapping;
+        }
+
+    }
+}
