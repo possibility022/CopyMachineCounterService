3d9b7c0ac0b8eddeede860c0ea9b9feaba1edd01
Tomasz Bąk <tomaszbakk@outlook.com>
2018-09-23 21:47:25 +0200

    Creating generic repository.

---
+++ MetCounterService/CopyinfoWPF.Repository/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CopyinfoWPF.Repository
+{
+    internal interface IGenericRepository<T> where T : class
+    {
+        bool Add(T entity);
+        bool Add(IEnumerable<T> items);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool Delete(IEnumerable<T> entities);
+        T FindBy(int id);
+        T FindBy<TV>(TV id);
+        IQueryable<T> All();
+        T FindBy(Expression<Func<T, bool>> expression);
+        IQueryable<T> FilterBy(Expression<Func<T, bool>> expression);
+    }
+}
