9bc57e2f1d1f99130604afff5fd8f14a3a5ecd47
Tomasz Bąk <tomaszbakk@outlook.com>
2019-05-01 18:11:10 +0200

    Adding CopyinfoWPF.Security.UnitTests project

---
+++ MetCounterService/CopyinfoWPF.Security.UnitTests/WinDpApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopyinfoWPF.Security.UnitTests
+{
+    [TestClass]
+    public class WinDpApi
+    {
+        [TestMethod]
+        public void Testing()
+        {
+        }
+    }
+}
