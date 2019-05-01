8c8b1c60766a90d529ff1f0f1ce72887509fe6e5
Tomasz Bąk <tomaszbakk@outlook.com>
2019-05-01 18:18:52 +0200

    Test for WinDpApi

---
+++ MetCounterService/CopyinfoWPF.Security.UnitTests/WinDpApiTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopyinfoWPF.Security.UnitTests
+{
+    [TestClass]
+    public class WinDpApiTests
+    {
+
+        WinDpApi _winDpApi;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _winDpApi = new WinDpApi();
+        }
+
+        [TestMethod]
+        public void Testing()
+        {
+            // Arrange
+            var stringToProtec = "ABC1234567890@#$%^&*(ERTYUIOCVBNMKJWA HOIDUAHWAIUHD";
+            var bytesToProtect = Encoding.UTF8.GetBytes(stringToProtec);
+            var @protected = _winDpApi.Protect(bytesToProtect);
+
+            // Act
+            var unprotected = _winDpApi.Unprotect(@protected);
+
+            var results = Encoding.UTF8.GetString(unprotected);
+
+            Assert.AreEqual(stringToProtec, results);
+        }
+    }
+}
